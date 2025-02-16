a2d3a0495bcadc33d022a89d31d75f133ecfa16a
Schkimansky <ahmadchawla1432@gmail.com>
2025-02-16 16:16:49 +0500

    Remove useless rendering, Improves lag spikes quite a bit.

--- scenes/main/fractalviewport.gd
+++ scenes/main/fractalviewport.gd
@@ -17,6 +17,8 @@ func _ready() -> void:
 	set_antialiasing(antialiasing)
 
 func set_antialiasing(target_aa: AntiAliasing) -> void:
+	antialiasing = target_aa
+	
 	if target_aa == AntiAliasing.TAA:
 		scaling_3d_mode = SCALING_3D_MODE_FSR2
 		screen_space_aa = SCREEN_SPACE_AA_DISABLED
@@ -29,22 +31,24 @@ func set_antialiasing(target_aa: AntiAliasing) -> void:
 
 func _process(delta: float) -> void:
 	var update_on: int = UPDATE_WHEN_VISIBLE if antialiasing == AntiAliasing.TAA else UPDATE_ONCE
-	if since_last_dynamic_update <= (0.75) / Engine.get_frames_per_second():
-		scaling_3d_scale = low_scaling
-		render_target_update_mode = UPDATE_WHEN_VISIBLE
-	elif scaling_3d_scale != high_scaling:
-		scaling_3d_scale = high_scaling
-		render_target_update_mode = UPDATE_DISABLED
+	var low_scaling_time: float = (0.75) / Engine.get_frames_per_second()
 	
 	var rendered_condition: bool = false
 	if antialiasing == AntiAliasing.TAA:
 		rendered_condition = since_last_dynamic_update >= 2.5
 	else:
-		rendered_condition = since_last_dynamic_update > 0.0
+		rendered_condition = since_last_dynamic_update > 0
 	
 	if rendered_condition:
 		render_target_update_mode = UPDATE_DISABLED
 	else:
+		render_target_update_mode = update_on
+	
+	if since_last_dynamic_update <= low_scaling_time:
+		scaling_3d_scale = low_scaling
+		render_target_update_mode = update_on
+	elif scaling_3d_scale != high_scaling:
+		scaling_3d_scale = high_scaling
 		render_target_update_mode = update_on
 	
 	since_last_dynamic_update += delta
